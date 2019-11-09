648cfcbc5bda140d5d3d916f5c53d5d7498b4277
Sean Williams <sewilli@microsoft.com>
2019-11-09 13:20:26 -0800

    Basic location filtering functionality.

--- SearchBot/Bot/Dialogs/Service/ServiceDialog.cs
+++ SearchBot/Bot/Dialogs/Service/ServiceDialog.cs
@@ -8,6 +8,12 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.IO;
+using System;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace SearchBot.Bot.Dialogs.Service
 {
@@ -70,13 +76,44 @@ namespace SearchBot.Bot.Dialogs.Service
             }
             */
 
-            var results = organizations.Where(o => o.Location == "");
+            string sub_key = "ay41VKwaVczc7rlvS9krCupc_OQybqGBLGFz9IsDZoc";
+            string query = conversationContext.Location;
+
+            var http = new HttpClient();
+            var url = string.Format("https://atlas.microsoft.com/search/fuzzy/json?api-version=1.0&subscription-key={0}&format=json&query={1}", sub_key, query);
+            var response = await http.GetAsync(url);
+            var result = await response.Content.ReadAsStringAsync();
+            var serializer = new DataContractJsonSerializer(typeof(RootObject));
+
+            var ms = new MemoryStream(Encoding.UTF8.GetBytes(result));
+            RootObject data = (RootObject)serializer.ReadObject(ms);
+
+            string userLat = Convert.ToString(data.results[0].position.lat);
+            string userLon = Convert.ToString(data.results[0].position.lon);
+
+            
+            var results = organizations.Where(o => CalcDistance(userLat, userLon, o.Latitude, o.Longitude) < 50.0).ToList();
 
 
             // If not, recommend multiple for the different needs.
+            string res = "";
+            for(int i = 0; i < results.Count; i++)
+            {
+                res = res + " " + Convert.ToString(results[i].Name);
+            }
+            return res;
+            //return $"Here's an organization that can help with {conversationContext.GetServicesString()} in {conversationContext.Location}!";
+        }
 
+        private double CalcDistance(string lat1, string lon1, string lat2, string lon2)
+        {
 
-            return $"Here's an organization that can help with {conversationContext.GetServicesString()} in {conversationContext.Location}!";
+            double R = 6371;
+            double dLat = (Math.PI / 180) * (Convert.ToDouble(lat2) - Convert.ToDouble(lat1));
+            double dLon = (Math.PI / 180) * (Convert.ToDouble(lon2) - Convert.ToDouble(lon1));
+            double a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) + Math.Cos((Math.PI / 180) * (Convert.ToDouble(lat1))) * Math.Cos((Math.PI / 180) * (Convert.ToDouble(lat2))) * Math.Sin(dLon / 2) * Math.Sin(dLon / 2);
+            double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+            return R * c;
         }
     }
 }
